4b24e0c27b9b79ce95674b606f8aebb8156f4031
Dylan Cashman <dylan.cashman@annkissam.com>
2012-04-23 21:25:45 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MothersDay::Application.config.secret_token = '3a0df8da76eda349313446cccb72833ba5485e8f0407fb9eb687c8c237fa452a6b7c6b17dd02cf3b52bc2622cdb7c72d1e47e9cf2b936f199b8c91ddd7a9e369'
